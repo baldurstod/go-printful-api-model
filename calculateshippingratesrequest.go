a04c1d2bf857dfcb80a1fa654e2d74d39a3b0a1d
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-10 22:34:30 +0100

    Updated CalculateShippingRatesRequest

--- calculateshippingratesrequest.go
+++ calculateshippingratesrequest.go
@@ -1,8 +1,12 @@
 package model
 
+import (
+	printfulmodel "github.com/baldurstod/go-printful-sdk/model"
+)
+
 type CalculateShippingRatesRequest struct {
-	Recipient AddressInfo `json:"recipient" bson:"recipient" mapstructure:"recipient"`
-	Items     []ItemInfo  `json:"items" bson:"items" mapstructure:"items"`
-	Currency  string      `json:"currency" bson:"currency" mapstructure:"currency"`
-	Locale    string      `json:"locale" bson:"locale" mapstructure:"locale"`
+	Recipient printfulmodel.ShippingRatesAddress                 `json:"recipient" bson:"recipient" mapstructure:"recipient"`
+	Items     []printfulmodel.CatalogOrWarehouseShippingRateItem `json:"items" bson:"items" mapstructure:"items"`
+	Currency  string                                             `json:"currency" bson:"currency" mapstructure:"currency"`
+	Locale    string                                             `json:"locale" bson:"locale" mapstructure:"locale"`
 }
