c2df5b49fbdd029be0d22266ef130caedfd813ac
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-14 17:04:50 +0100

    Added CreateOrderResponse

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/baldurstod/go-printful-api-model
 
 go 1.22.4
 
-require github.com/baldurstod/go-printful-sdk v0.2.11
+require github.com/baldurstod/go-printful-sdk v0.2.17
--- responses/order.go
+++ responses/order.go
@@ -2,9 +2,18 @@ package responses
 
 import (
 	"github.com/baldurstod/go-printful-api-model/schemas"
+	printfulmodel "github.com/baldurstod/go-printful-sdk/model"
 )
 
 type Order struct {
 	Code   int           `json:"code" bson:"code" mapstructure:"code"`
 	Result schemas.Order `json:"result" bson:"result" mapstructure:"result"`
+}
+
+type CreateOrderResponse struct {
+	Success bool `json:"success" bson:"success" mapstructure:"success"`
+	Result  struct {
+		Order printfulmodel.Order `json:"order" bson:"order" mapstructure:"order"`
+	} `json:"result" bson:"result" mapstructure:"result"`
+	Error `json:"error" bson:"error" mapstructure:"error"`
 }
